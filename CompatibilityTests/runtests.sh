f7c1f0c82697535961abdf9e8f4b2bb8c3565ec7
zenarcher007 <jdouty03@icloud.com>
2024-06-01 12:27:48 -0500

    Change docker inspect command, use gh action docker build caching?

--- CompatibilityTests/runtests.sh
+++ CompatibilityTests/runtests.sh
@@ -22,12 +22,12 @@ fi
 function master_test() {
   OS="$1"; TERM="$2"; COMPILER="$3"; CPPSTANDARD="$4"
   # Check if base image currently exists:
-  grep 'INSPECT_TRUE' >/dev/null <<< "$($DOCKER inspect "test_termcalc_base_$OS":latest && echo INSPECT_TRUE || echo INSPECT_FALSE)"
+  grep 'INSPECT_TRUE' >/dev/null <<< "$($DOCKER image inspect "test_termcalc_base_$OS":latest && echo INSPECT_TRUE || echo INSPECT_FALSE)"
   if [[ "$?" == "0" ]]; then
     echo  "  > Base image for $OS already exists, skipping build."
   else
     echo  "  > Building base image... "
-    $DOCKER  build  -t  "test_termcalc_base_$OS"  --build-arg OS="$OS"  --build-arg TERM="$TERM"  --build-arg COMPILER="$COMPILER"  --build-arg CPPSTANDARD="$CPPSTANDARD"  -f  "CompatibilityTests/Dockerfile.base_$OS" .
+    $DOCKER  build  -t  "test_termcalc_base_$OS"  --build-arg OS="$OS"  --build-arg TERM="$TERM"  --build-arg COMPILER="$COMPILER"  --build-arg CPPSTANDARD="$CPPSTANDARD"  $EXTRA_DOCKER_BUILD_ARGS -f  "CompatibilityTests/Dockerfile.base_$OS" .
     if [[  "$?"  != 0 ]]; then
       echo  >&2  "ERROR: Failed to build base image for OS: $OS, TERM: $TERM, COMPILER: $COMPILER, CPPSTANDARD: $CPPSTANDARD"
       exit  1
@@ -39,7 +39,7 @@ function master_test() {
   mkdir -p "/tmp/termcalc_docker_build_logs"
   dirId="os_$OS_term_$TERM_compiler_$COMPILER_std_$CPPSTANDARD_$(uuidgen)"
   opposite_compiler="$(if [ "${COMPILER}" = "clang++" ]; then echo g++; else echo clang++; fi)"
-  $DOCKER build -f "CompatibilityTests/Dockerfile.$OS" -t "test_termcalc_build_$(tr '[:upper:]' '[:lower:]' <<< "$OS")" . --build-arg OPPOSITE_COMPILER="$opposite_compiler" --build-arg TERM="$TERM" --build-arg COMPILER="$COMPILER" --build-arg CPPSTANDARD="$CPPSTANDARD" &>"/tmp/termcalc_docker_build_logs/$dirId.log" && echo "IMAGE BUILT SUCCESSFULLY!"
+  $DOCKER build -f "CompatibilityTests/Dockerfile.$OS" -t "test_termcalc_build_$(tr '[:upper:]' '[:lower:]' <<< "$OS")" . --build-arg OPPOSITE_COMPILER="$opposite_compiler" --build-arg TERM="$TERM" --build-arg COMPILER="$COMPILER" --build-arg CPPSTANDARD="$CPPSTANDARD" $EXTRA_DOCKER_BUILD_ARGS &>"/tmp/termcalc_docker_build_logs/$dirId.log" && echo "IMAGE BUILT SUCCESSFULLY!"
   if [[ $? -ne 0 ]]; then
     echo "TEST FAILED! Here are the logs:"
     cat "/tmp/termcalc_docker_build_logs/$dirId.log"
